e916ab60ee34d5e3e6f9c09be0618caa64cf6eed
alonso <programador@tecnimotors.com>
2024-10-17 18:31:52 -0500

    actualizacion book html angular

--- src/app/body/listfilter/listfilter.component.ts
+++ src/app/body/listfilter/listfilter.component.ts
@@ -3,16 +3,21 @@ import { RouterLink } from '@angular/router';
 import { LecturaService } from '../../core/service/lectura.service';
 import { CommonModule } from '@angular/common';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-listfilter',
   standalone: true,
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, FormsModule],
   templateUrl: './listfilter.component.html',
   styleUrl: './listfilter.component.css',
 })
-export class ListfilterComponent {
+export class ListfilterComponent implements OnInit {
   public dtltipo: string = '';
+  public lstProducto: any[] = [];
+  public txtcatidioma: string = 'Español';
+  public txtsearchtitulo: string = '';
+
   constructor(
     private lecturaService: LecturaService,
     private route: ActivatedRoute,
@@ -21,7 +26,30 @@ export class ListfilterComponent {
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
-      this.dtltipo = params['tipo'];
+      this.dtltipo = params['ibic'];
+      this.VoidListadoProducto();
     });
+  }
+
+  VoidListadoProducto() {
+    this.lecturaService
+      .getfilterclasificacionIdioma(this.txtcatidioma, this.dtltipo)
+      .subscribe({
+        next: (dtl: any) => {
+          console.log(dtl);
+          this.lstProducto = dtl;
+        },
+        error: () => {},
+      });
+  }
+  busquedaTitulo() {
+    this.lecturaService
+      .getSearchtituloidioma(this.txtcatidioma, this.txtsearchtitulo)
+      .subscribe({
+        next: (dtl: any) => {
+          this.lstProducto = dtl;
+        },
+        error: () => {},
+      });
   }
 }
--- src/app/core/service/lectura.service.ts
+++ src/app/core/service/lectura.service.ts
@@ -4,14 +4,16 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LecturaService {
   private baseUrl = environment.mylectura;
   private promocionDiario = '/bmg_books/promocion_diaria';
   private bmgbookid = '/bmg_books/book';
-  private bmgBooksClassification = '/bmg_books/classifications/books/A/language/Español'; // Nueva propiedad
-
+  private bmgBooksClassification =
+    '/bmg_books/classifications/books/A/language/Español'; // Nueva propiedad
+  private filterclasificacionIdioma = '/bmg_books/classifications/books';
+  private Searchtituloidioma = '/bmg_books/search_by_language_and_title';
 
   constructor(private http: HttpClient) {}
   getPromocionDiario(): Observable<any> {
@@ -24,12 +26,37 @@ export class LecturaService {
     return this.http.get(this.baseUrl + this.bmgbookid + '/' + idarti);
   }
 
-  getBooksClassification(): Observable<any> {  // Nuevo método
-    return this.http.get(this.baseUrl + this.bmgBooksClassification + '?limit=100&offset=0');
+  getBooksClassification(): Observable<any> {
+    // Nuevo método
+    return this.http.get(
+      this.baseUrl + this.bmgBooksClassification + '?limit=100&offset=0'
+    );
+  }
+  getfilterclasificacionIdioma(
+    idioma: string,
+    clasificacion: string
+  ): Observable<any> {
+    // Nuevo método
+    return this.http.get(
+      this.baseUrl +
+        this.filterclasificacionIdioma +
+        '/' +
+        clasificacion +
+        '/language/' +
+        idioma +
+        '?limit=100&offset=0'
+    );
+  }
+  getSearchtituloidioma(idioma: string, titulo: string): Observable<any> {
+    // Nuevo método
+    return this.http.get(
+      this.baseUrl +
+        this.Searchtituloidioma +
+        '?idioma=' +
+        idioma +
+        '&titulo=' +
+        titulo +
+        '&limit=100&offset=0'
+    );
   }
 }
-
-
-
-
-

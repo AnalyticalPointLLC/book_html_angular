3e91e9d20a311c05ba5e983b6e432aa585fcb6c4
marcelohumerez <marcelohumerez@gmail.com>
2024-10-20 17:05:10 -0500

    se actualiza menu lateral izquierdo, se instala paginacion, se mejora menu principal

--- src/app/body/listfilter/listfilter.component.ts
+++ src/app/body/listfilter/listfilter.component.ts
@@ -5,6 +5,11 @@ import { CommonModule } from '@angular/common';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
+interface Categoria {
+  codigo_ibic: string;
+  nombre_clasificacion: string;
+}
+
 @Component({
   selector: 'app-listfilter',
   standalone: true,
@@ -18,6 +23,7 @@ export class ListfilterComponent implements OnInit {
   public txtcatidioma: string = 'Español';
   public txtsearchtitulo: string = '';
   public nombreCategoria: string = '';
+  public categorias: Categoria[] = [];
 
   constructor(
     private lecturaService: LecturaService,
@@ -26,6 +32,33 @@ export class ListfilterComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+
+
+
+      this.categorias = [
+        { codigo_ibic: 'A', nombre_clasificacion: 'Las artes' },
+        { codigo_ibic: 'B', nombre_clasificacion: 'Biografía e historias reales' },
+        { codigo_ibic: 'C', nombre_clasificacion: 'Lenguaje' },
+        { codigo_ibic: 'D', nombre_clasificacion: 'Literatura y estudios literarios' },
+        { codigo_ibic: 'E', nombre_clasificacion: 'Enseñanza de la lengua inglesa' },
+        { codigo_ibic: 'F', nombre_clasificacion: 'Ficción y temas afines' },
+        { codigo_ibic: 'G', nombre_clasificacion: 'Consulta, información y temas interdisciplinarios' },
+        { codigo_ibic: 'H', nombre_clasificacion: 'Humanidades' },
+        { codigo_ibic: 'J', nombre_clasificacion: 'Sociedad y ciencias sociales' },
+        { codigo_ibic: 'K', nombre_clasificacion: 'Economía, finanzas, empresa y gestión' },
+        { codigo_ibic: 'L', nombre_clasificacion: 'Derecho' },
+        { codigo_ibic: 'M', nombre_clasificacion: 'Medicina' },
+        { codigo_ibic: 'P', nombre_clasificacion: 'Matemáticas y ciencia' },
+        { codigo_ibic: 'R', nombre_clasificacion: 'Ciencias de la tierra, geografía, medioambiente, planificación' },
+        { codigo_ibic: 'T', nombre_clasificacion: 'Tecnología, ingeniería, agricultura' },
+        { codigo_ibic: 'U', nombre_clasificacion: 'Computación e informática' },
+        { codigo_ibic: 'V', nombre_clasificacion: 'Salud y desarrollo personal' },
+        { codigo_ibic: 'W', nombre_clasificacion: 'Estilo de vida, deporte y ocio' },
+        { codigo_ibic: 'Y', nombre_clasificacion: 'Infantiles, juveniles y didácticos' }
+      ];
+
+
+
     this.route.params.subscribe((params) => {
       this.dtltipo = params['ibic'];
       this.nombreCategoria = params['nombreCategoria']; // Obtener el nombre de la categoría
@@ -63,6 +96,13 @@ export class ListfilterComponent implements OnInit {
         error: () => {},
       });
   }
+
+  onCategoriaClick(categoria: Categoria) {
+    this.dtltipo = categoria.codigo_ibic;
+    this.nombreCategoria = categoria.nombre_clasificacion;
+    this.VoidListadoProducto();
+  }
+
   busquedaTitulo() {
     this.lecturaService
       .getSearchtituloidioma(this.txtcatidioma, this.txtsearchtitulo)

d5c9e83ee27cbfa8495fe2ccc310ddab8e86994f
marcelohumerez <marcelohumerez@gmail.com>
2024-10-21 00:08:41 -0500

    se actualiza menu principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet, RouterLink, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms'; // Importa FormsModule
 
 
@@ -15,9 +15,18 @@ export class AppComponent {
   title = 'libreria_4_html';
   txtsearchtitulo: string = '';
 
+
+  constructor(private router: Router) {}
+
+
   busquedaTitulo() {
     console.log('Buscando título:', this.txtsearchtitulo);
     // Implementa la lógica de búsqueda aquí
+  }
+
+  navigateToNuevos() {
+    // Navega a la ruta con los parámetros para cargar los nuevos títulos
+    this.router.navigate(['/listfilter', 'NuevosTitulos', 'Nuevos Títulos']);
   }
 
 }
